92b1220ebaba7ddf2706286688123cbbb5dce12e
edxco <eduardo.baeza@yahoo.com>
2020-11-03 16:25:38 -0600

    Creating, editing and deleting tags

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,17 @@
+class TagsController < ApplicationController
+  def show
+    @tag = Tag.find(params[:id])
+  end
+  def index
+    @tags = Tag.all
+  end
+  def destroy
+    @tag = Tag.find(params[:id])
+    p @tag
+    @tag.destroy
+
+    flash.notice = "Tag '#{@tag.name}' was deleted!"
+
+    redirect_to tags_path
+  end
+end
